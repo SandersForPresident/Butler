e0afbffc27d8240bb227ebef56b41ae73feb286a
Atticus White <contact@atticuswhite.com>
2015-08-25 05:05:06 -0400

    enviornment var

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var Slack = require('slack-client'),
-    Bot = require('./src/bot'),
-    config = require('./.config.env.json');
+    Bot = require('./src/bot');
 
 (function () {
-  var slack = new Slack(config.slack.key, true),
-      bot = new Bot(slack);
+  var token = process.env.SLACK_TOKEN,
+      slack,
+      bot;
+
+  if (!token) {
+    throw new Error('Slack token is required. Please provide SLACK_TOKEN as an environment variable');
+  }
+
+  slack = new Slack(config.slack.key, true);
+  bot = new Bot(slack);
 }).call(this);
