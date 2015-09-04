565aac41bfbaedf2827d67bf9fbb299878f6c64b
Atticus White <contact@atticuswhite.com>
2015-09-04 01:25:21 -0400

    test directory

---
+++ test/spec/bot.js
@@ -0,0 +1,14 @@
+describe('Bot', function () {
+  var chai = require('chai'),
+      sinon = require('sinon'),
+      Bot = require('../../src/bot'),
+      expect,
+      should;
+  before(function () {
+    expect = chai.expect;
+    should = chai.should;
+  });
+  it ('should do something', function () {
+    // @TODO
+  });
+});
